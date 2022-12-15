34578d04dbe3affc3532c19550c20d6d8a0b3b3c
Samir Medjdoub <samirmedjdoub@imac-de-samir-1.home>
2022-12-15 12:21:13 +0100

    Generate a PDF

--- src/pages/MyEstimates.tsx
+++ src/pages/MyEstimates.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { EstimateCtx } from '../App'
 import { EstimateService } from '../services/estimateService'
+import PDFprinter from '../services/PDFprinter'
 
 export default function MyEstimates() {
     const estimateSrv = useContext<EstimateService>(EstimateCtx)
@@ -9,6 +10,8 @@ export default function MyEstimates() {
     return <>
         <h3>All my estimates</h3>
         <div>TODO list all estimates</div>
-        {JSON.stringify(estimates, null, 2)}
+        <PDFprinter>
+            {JSON.stringify(estimates, null, 2)}
+        </PDFprinter>
     </>
 }
