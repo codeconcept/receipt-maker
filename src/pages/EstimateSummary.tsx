d10d50a147f444930bb1cabdff9b6a08760ed203
Samir Medjdoub <samirmedjdoub@imac-de-samir-1.home>
2022-12-16 16:01:51 +0100

    Style EstimateSummary

--- src/pages/EstimateSummary.tsx
+++ src/pages/EstimateSummary.tsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import { Estimate } from '../App';
+import styles from './EstimateSummary.module.css'
 
 type estimateSummaryProps = {
     data: Estimate;
 }
 
 export default function EstimateSummary({ data }: estimateSummaryProps) {
-    return (<div>
+    return (<div className={styles.summary}>
         <h4>{data.title}</h4>
         <div>Estimate date: {new Date(data.estimateDate).toLocaleDateString("en-US")}</div>
         <Link to={`/my-estimates/${data?.id}`}>see details</Link>
--- src/pages/MyEstimates.tsx
+++ src/pages/MyEstimates.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { EstimateCtx } from '../App'
 import { EstimateService } from '../services/estimateService'
 import EstimateSummary from './EstimateSummary'
+import styles from './MyEstimates.module.css';
 
 export default function MyEstimates() {
     const estimateSrv = useContext<EstimateService>(EstimateCtx)
@@ -9,7 +10,7 @@ export default function MyEstimates() {
 
     return <>
         <h3>All my estimates</h3>
-        <div>
+        <div className={styles.estimatelist}>
             {estimates.map(est => (<EstimateSummary data={est} key={est.id} />))}
         </div>
     </>
